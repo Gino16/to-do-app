3ba0db65a5afc5278c61800940c1de28166658b8
Gino Ascencio <gino.francisco@hotmail.com>
2025-03-02 14:49:29 -0500

    init project

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// Its server.js on node.js
+// It is a simple server to make CRUD operations of to do list
+// It uses express.js to handle the requests and responses
+// It uses body-parser to parse the request body
+// It uses mongose to connect to the mongoDB database
+// It uses cors to allow the requests from the client
+// It uses dotenv to load the environment variables
+// It uses nodemon to restart the server on file changes
+// It uses nodemon.json to configure nodemon
+// It uses .env to store the environment variables
+// It uses .gitignore to ignore the node_modules folder
+// It uses package.json to store the dependencies
+// It uses package-lock.json to store the exact version of the dependencies
+// It uses README.md to store the project documentation
+// It uses .env.example to store the example environment variables
